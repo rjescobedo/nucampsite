f74f215df380d8052116bd1c06beb4a58da26ca5
Raul Escobedo <rjescobedo@gmail.com>
2021-11-20 10:02:10 -0700

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(function() {
+    //Carousel
     $(".carousel").carousel( {interval: 2000} );
     $("#carouselButton").click(function(){
         if ($("#carouselButton").children("i").hasClass("fa-pause")) {
@@ -10,5 +11,15 @@ $(function() {
             $("#carouselButton").children("i").removeClass("fa-play");
             $("#carouselButton").children("i").addClass("fa-pause");
         }
+    });
+
+    //Reserve Campsite Modal
+    $("#reserveButton").click(function() {
+        $("#reserveModal").modal("show");
+    });
+
+    //Login Modal
+    $("#loginButton").click(function() {
+        $("#loginModal").modal("show");
     });
 });
